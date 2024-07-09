aa87590be1a4298eb90572b9a46d157a0c4fa857
Ryan Milton <mr.ryan.milton@gmail.com>
2024-07-08 22:16:34 -0700

    Storybook

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,11 @@
-export default function Button() {
-  return <button className="text-pink-500">Feedback</button>;
+import { ComponentProps } from "react";
+
+export type ButtonProps = ComponentProps<"button">;
+
+export default function Button({ children, ...props }: ButtonProps) {
+  return (
+    <button className="text-pink-500" {...props}>
+      {children}
+    </button>
+  );
 }
